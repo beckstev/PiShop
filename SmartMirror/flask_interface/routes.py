959366a4c4a0defcdb87f7ff135d55746b105d17
beckstev <beckstev@web.de>
2019-03-30 19:32:31 +0100

    We are now able to display connections of a specific station

--- SmartMirror/flask_interface/routes.py
+++ SmartMirror/flask_interface/routes.py
@@ -1,7 +1,12 @@
 import flask
 from flask_interface import app
+from vrr_api.vrr_api import get_station_information
 
 
 @app.route('/')
-def hello_world():
-    return flask.render_template('hello_world.html')
+def smart_mirror():
+    station = 'Dortmund Brackel'
+    connections = get_station_information(station, 4)
+
+    return flask.render_template('smart_mirror.html', station=station,
+                                  connections=connections)
