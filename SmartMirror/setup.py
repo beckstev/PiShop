c709e1d9719e49adfb44a0efb771347297aab1db
beckstev <beckstev@web.de>
2019-03-30 22:18:54 +0100

    added google_api to pip packages

--- SmartMirror/setup.py
+++ SmartMirror/setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 
 setup(name='vrr_api',
-      version='0.9',
+      version='1.0',
       description='Use the VRR API to get realtime information about connections at a spezific station.',
       author='Karl Schiller & Steven Becker',
       packages=['vrr_api'])
+
+setup(name='google_calendar_api',
+      version='0.9',
+      description='Use the google calander api to get the events of the day',
+      author='Google, Karl Schiller & Steven Becker',
+      packages=['google_calendar_api'])
