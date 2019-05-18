0016dfbc9716aaf0458f45fba71362cd6872513c
beckstev <beckstev@web.de>
2019-05-18 10:25:55 +0200

    Created flask tutorial

---
+++ tutorial/flask/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/my_html')
+def my_function():
+    return render_template('base.html', title='Was geht')
+
+
+@app.route('/my_image')
+def my_image():
+    image_name = 'Hackathon_SmartHome2019.png'
+    return render_template('image.html', image=image_name)
